3b97c9b1fdf1df2c7870e680c2ee83b47864921b
Adam Wolnikowski <awolnikowski@gmail.com>
2020-01-05 11:58:12 -0500

    Shooter init

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,4 +11,14 @@ public class Constants {
 
     public static double indexer_speed = 0.8;
     public static double feeder_speed = 0.8;
+
+    // Shooter constants
+    public static double shooter_kP = 0.00005;
+    public static double shooter_kI = 0.000001;
+    public static double shooter_kD = 0;
+    public static double shooter_kIz = 0;
+    public static double shooter_kFF = 0;
+    public static double shooter_kMaxOutput = 1;
+    public static double shooter_kMinOutput = -1;
+    public static double shooter_maxRPM = 5700;
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,19 +2,45 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.ControlType;
 
 import frc.robot.PortMap;
+import frc.robot.Constants;
 
 public class Shooter {
-    CANSparkMax[] shooterMotors = new CANSparkMax[] {
+    private Constants constants;
+
+    private CANSparkMax[] shooterMotors = new CANSparkMax[] {
         new CANSparkMax(PortMap.SHOOTER_CAN[0], MotorType.kBrushless),
         new CANSparkMax(PortMap.SHOOTER_CAN[1], MotorType.kBrushless),
     };
 
+    private CANPIDController pidController;
+    private CANEncoder encoder;
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+
     public Shooter() {
+        constants = Constants.getConstants();
+
         for (CANSparkMax spark : shooterMotors) {
             spark.setIdleMode(CANSparkMax.IdleMode.kCoast);
         }
+
+        pidController = shooterMotors[0].getPIDController();
+        encoder = shooterMotors[0].getEncoder();
+
+        // PID coefficients
+        kP = Constants.shooter_kP;
+        kI = Constants.shooter_kI;
+        kD = Constants.shooter_kD;
+        kIz = Constants.shooter_kIz;
+        kFF = Constants.shooter_kFF;
+        kMaxOutput = Constants.shooter_kMaxOutput;
+        kMinOutput = Constants.shooter_kMinOutput;
+        maxRPM = Constants.shooter_maxRPM;
+
 
     }
 }
